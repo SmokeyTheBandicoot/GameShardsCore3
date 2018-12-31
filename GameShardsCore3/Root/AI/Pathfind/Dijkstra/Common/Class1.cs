000219a1ea821d79f5f52104cb09625ced5eb766
miky gargiulo <miki.gargiulo@live.it>
2018-12-31 11:36:10 +0100

    Aggiungere i file di progetto.

---
+++ GameShardsCore3/Root/AI/Pathfind/Dijkstra/Common/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Pathfind.Dijkstra.Common {
+
+    /// <summary>
+    /// Class which contains info about Graph events
+    /// </summary>
+    public class GraphEventArgs : EventArgs {
+
+        public GraphInvalidationReason InvalidationReason { get; set; }
+
+        public GraphEventArgs(GraphInvalidationReason reason) {
+            this.InvalidationReason = reason;
+        }
+
+        public enum GraphInvalidationReason {
+            OnNodes,
+            OnEdges
+        }
+    }
+}
