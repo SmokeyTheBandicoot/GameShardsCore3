3e815ea2c7c477fa9ac83f9b703a0ab4791bd436
miky gargiulo <miki.gargiulo@live.it>
2019-11-13 19:23:25 +0100

    commit 2019_NOV_13_A Addition of Levenshtein Transcript Distance

--- GameShardsCore3/StringManipulation/StringManipulator.cs
+++ GameShardsCore3/StringManipulation/StringManipulator.cs
@@ -32,6 +32,72 @@ namespace GameShardsCore3.StringManipulation {
             return res;
         }
 
+        public double LevenshteinDistance(String s1, String s2) {
+
+            int len1 = s1.Length;
+            int len2 = s2.Length;
+
+            int[,] matrix = new int[len1, len2];
+
+            for (int x = 1; x < len1; x++) {
+                for (int y = 1; y < len2; y++) {
+                    matrix[x, y] = 0;
+                }
+            }
+
+            for (int i = 1; x <= len1; x++)
+                matrix[i, 0] = i;
+            
+
+            for (int j = 0; y <= len2; j++)
+                matrix[0, j] = j;
+            
+            for (int i = 1; i <= len2(s1); i++) 
+                for (int j = 1; j <= len2; j++) {
+
+                    int offset = 0;
+                    if (s1[i] != s2[j]) offset = 1;
+
+                    matrix[i, j] = Math.min(matrix[i - 1, j - 1] + offset, 
+                        Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1));
+                }
+            
+            return matrix[len1, len2];
+        }
+
+        public double[,] LevenshteinMatrix(String s1, String s2) {
+
+            double len1 = s1.Length;
+            double len2 = s2.Length;
+
+            double[,] matrix = new double[len1, len2];
+
+            for (double x = 1; x < len1; x++) {
+                for (double y = 1; y < len2; y++) {
+                    matrix[x, y] = 0;
+                }
+            }
+
+            for (double i = 1; x <= len1; x++)
+                matrix[i, 0] = i;
+            
+
+            for (double j = 0; y <= len2; j++)
+                matrix[0, j] = j;
+            
+            for (double i = 1; i <= len2(s1); i++) 
+                for (double j = 1; j <= len2; j++) {
+
+                    double offset = 0;
+                    if (s1[i] != s2[j]) offset = 1;
+
+                    matrix[i, j] = Math.min(matrix[i - 1, j - 1] + offset, 
+                        Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1));
+                }
+            
+            return matrix;
+        }
+
     }
 
 }
